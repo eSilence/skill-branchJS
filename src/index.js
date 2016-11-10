d54e60754b27cd94c530af918d74e17385333df5
eSilence <eSilence@yandex.ru>
2016-11-10 13:51:39 +0300

    add HW 2C

--- src/index.js
+++ src/index.js
@@ -1,34 +1,55 @@
 import express from 'express';
 import cors from 'cors';
 
-var transName = function(fullName){
-  try{
-   console.log("> " + fullName);
-   if (fullName == '' | fullName == undefined || fullName == " " )
-    return "Invalid fullname";
 
-    console.log((/\d/).test(fullName));
-    if ((/\d/).test(fullName))
-      return "Invalid fullname";
+// var transName = function(fullName){
+//   try{
+//    console.log("> " + fullName);
+//    if (fullName == '' | fullName == undefined || fullName == " " )
+//     return "Invalid fullname";
+//
+//     console.log((/\d/).test(fullName));
+//     if ((/\d/).test(fullName))
+//       return "Invalid fullname";
+//
+//    var reg =  /([^\s]*)?\s?([^\s]*)?\s?([^\s]*)?\s?([^\s]*)?/;// /([\wа-яА-Я]*)?\s?([\wа-яА-Я]*)?\s?([\wа-яА-Я]*)?\s?([\wа-яА-Я]*)?/;
+//    var name = fullName.match(reg);
+//    console.log(name);
+//
+//
+//   if (name[2]==undefined)
+//     name = name[1];
+//   else if (name[3]==undefined)
+//       name = name[2]+' '+name[1][0]+'.';
+//   else if (name[4] ==undefined)
+//    name = name[3]+' '+name[1][0]+'. '+ name[2][0]+'.';
+//    else name = "Invalid fullname";
+//    console.log(name);
+//    return name;
+//  }
+//  catch (e){
+//    return e.name;
+//  }
+// }
+var lookGitHub = function(url){
+  return /github.com/i.test(url);
+}
 
-   var reg =  /([^\s]*)?\s?([^\s]*)?\s?([^\s]*)?\s?([^\s]*)?/;// /([\wа-яА-Я]*)?\s?([\wа-яА-Я]*)?\s?([\wа-яА-Я]*)?\s?([\wа-яА-Я]*)?/;
-   var name = fullName.match(reg);
-   console.log(name);
+var getUserName = function(url){
+  console.log("> " + url);
+  if (lookGitHub(url)){
+    var reg = /(github.com\/)([\w]*)/;
+    var userName = url.match(reg)[2];
+  }
+  else{
+  url = url.slice(url.lastIndexOf("/")+1);
+    console.log(">> " + url);
+    reg = /@?[\w]*/;
+    var userName = url.match(reg)[0];
+}
 
-
-  if (name[2]==undefined)
-    name = name[1];
-  else if (name[3]==undefined)
-      name = name[2]+' '+name[1][0]+'.';
-  else if (name[4] ==undefined)
-   name = name[3]+' '+name[1][0]+'. '+ name[2][0]+'.';
-   else name = "Invalid fullname";
-   console.log(name);
-   return name;
- }
- catch (e){
-   return e.name;
- }
+  console.log(userName);
+  return (userName[0]=='@')?userName:'@' + userName;
 }
 
 const app = express();
@@ -51,6 +72,12 @@ app.get('/task2B', (req, res) => {
 
 });
 
+app.get('/task2C', (req, res) => {
+  var url = req.query.username;
+  //url = "www.w.w/qw12323/webstick?ertretret.ert$dgfdg";
+  res.send(getUserName(url));
+
+});
 app.listen(3000, () => {
   console.log('Your app listening on port 3000!');
 });
